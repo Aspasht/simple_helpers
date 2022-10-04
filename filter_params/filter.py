5d3cd931a904e653dd88881f9dbf457b2137ae1e
Aspasht <sykoman944@gmail.com>
2022-10-04 17:25:33 +0300

    params filtering scripts added

---
+++ filter_params/filter.py
@@ -0,0 +1,55 @@
+#!/bin/python3
+
+'''
+    This script take a file containing urls as an argument and find and filter
+    query params.
+'''
+
+from urllib.parse import urlparse, unquote
+import sys
+
+
+fetched_paths = []
+fetched_queries = []
+params = []
+
+file = sys.argv[1]
+def file_input(file):
+    with open(file) as f:
+        for line in f:
+            link = urlparse(line)
+            url_path = link.path
+            queries = unquote(link.query)
+            if (url_path != "/") and (url_path != ""):
+                fetched_paths.append(url_path)
+            if (queries != "/") and (queries != ""):
+                fetched_queries.append(queries)
+
+
+def grab_params(x):
+    for i in x:
+        v = i.partition("=")
+        params.append(v[0])
+
+
+def uniq_param(data):
+    list_set = set(data)
+    uniq_value = (list(filter(None,list_set)))
+    for value in uniq_value:
+        if len(value) < 10:
+            if not value.isnumeric():
+                if value != "":
+                    if (value != urlparse(value)):
+                        x=value.strip("\\,.,/,?,' ',=,-")
+                        print(x.strip())
+
+
+# grab_params(fetched_queries)
+# uniq_param(params)
+
+
+if __name__== "__main__":
+    if sys.argv[1]:
+        file_input(sys.argv[1])
+        grab_params(fetched_queries)
+        uniq_param(params)
